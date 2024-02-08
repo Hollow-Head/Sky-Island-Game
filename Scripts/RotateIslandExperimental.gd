91bb2a212bea32063817461aa5a1ef7ce4ad7aac
Notherman <134525115+NothermanVEVO@users.noreply.github.com>
2024-02-08 20:36:32 -0300

    Added Rotation and Translation

--- Scripts/RotateIslandExperimental.gd
+++ Scripts/RotateIslandExperimental.gd
@@ -5,27 +5,41 @@ extends Node
 @onready var point : Marker2D = get_node("Point")
 @onready var island : Sprite2D = get_node("Island")
 
-var angle := 0.0
+var translationAngle := 0.0
 
-@export var clockwise := true
+@export var translationClockwise := true
 @export var radius := 400.0
-@export var speed := 1
-@export var startAngle := 0
+@export var translationSpeed := 1.0
+@export var translationStartAngle := 0
 
-# Called when the node enters the scene tree for the first time.
+var rotationAngle := 0.0
+
+@export var rotationClockwise := true
+@export var rotationSpeed := 1.0
+@export var rotationStartAngle := 0
+
 func _ready():
-	angle -= deg_to_rad(startAngle)
+	translationAngle -= deg_to_rad(translationStartAngle)
+	rotationAngle -= deg_to_rad(rotationStartAngle)
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	orbitTranslation(delta)
 	orbitRotation(delta)
 	pass
 
-func orbitRotation(delta : float):
-	if clockwise:
-		angle += delta
+func orbitTranslation(delta : float):
+	if translationClockwise:
+		translationAngle += delta
 	else:
-		angle -= delta
-	island.global_position = Vector2(cos(angle * speed) * radius, 
-									sin(angle * speed) * radius) + point.global_position
+		translationAngle -= delta
+	island.global_position = Vector2(cos(translationAngle * translationSpeed) * radius, 
+									sin(translationAngle * translationSpeed) * radius) + point.global_position
+
+func orbitRotation(delta : float):
+	if rotationClockwise:
+		rotationAngle += delta
+	else:
+		rotationAngle -= delta
+	island.global_rotation = rotationAngle * rotationSpeed
+	pass
